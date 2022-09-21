1f4e6e6569fd79d8e4edb2e517da2040cc47226a
NatiG <nati.g5684@gmail.com>
2022-09-21 19:26:16 +0300

    Install linters & fix errors

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::MessagesController < ApplicationController
   def index
     @messages = Message.first.text
     render json: {
-        text: @messages
+      text: @messages
     }
   end
 end
